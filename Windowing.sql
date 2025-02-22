a8bedd216dd79efdba9032304cf69fa0e1867449
HCThysen <heidi.thysen@gmail.com>
2025-02-21 21:45:12 -0800

    More windowing functions

--- Windowing.sql
+++ Windowing.sql
@@ -15,3 +15,25 @@ SELECT "group", id, value, AVG(value) OVER(PARTITION BY "Group"
     ORDER BY id ASC ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) as group_mavg
 FROM "movingAvg2";
 
+-- Create a column that shows the rank value for each group
+SELECT id, "group", value, RANK() 
+    OVER(PARTITION BY "group" 
+    ORDER BY value ASC) as rank
+FROM rank_table2;
+-- NULL in this type of rank is treated as the largest
+-- Equal values are given the same rank, but jumps that many numbers after
+
+SELECT id, "group", value, RANK_DENSE() 
+    OVER(PARTITION BY "group" 
+    ORDER BY value ASC) as rank
+FROM rank_table2;
+-- the next is rank is given the number right after the rank with multiple values attached
+
+SELECT id, "group", value, PERCENT_RANK() 
+    OVER(PARTITION BY "group" 
+    ORDER BY value ASC) as rank
+FROM rank_table2;
+--Gives the relative ranking from 0 to 1
+
+-- create a (high medium low) grouping column that depends upon the sum of values in value1 and value2.
+-- Low if the sum is strictly less than 20, medium if the value is bigger tahn 20 but strictly less than 40 and otherwise high
